9f5981a1d7400b4a86a72840d5ce3bf562070fa0
Kudelich Dmitry <dima.kudelich2@mail.ru>
2021-01-05 13:19:56 +0300

    fix bug

--- Framework/src/test/java/driver/DriverSingleton.java
+++ Framework/src/test/java/driver/DriverSingleton.java
@@ -3,6 +3,7 @@ package driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
 
@@ -12,9 +13,16 @@ public class DriverSingleton {
 
     public static WebDriver getDriver(){
         if (null == driver){
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-
+            switch (System.getProperty("browser")){
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+            }
             driver.manage().window().maximize();
         }
         return driver;
